01cf26fe2312b28f9dec9fbbd13c91327dc67ff7
IvanKustarev <ivan.kustarev@gmail.com>
2021-10-31 11:22:32 +0300

    Initial commit

---
+++ src/main/java/com/example/penalties/PenaltiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.penalties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenaltiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenaltiesApplication.class, args);
+    }
+
+}
